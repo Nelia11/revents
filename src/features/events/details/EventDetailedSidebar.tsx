a6e2715755fe02edbb50849adced17697b6f233f
Nelia11 <levit.nelia@icloud.com>
2024-01-17 20:50:35 +0100

    Add Detailed Sidebar

--- src/features/events/details/EventDetailedSidebar.tsx
+++ src/features/events/details/EventDetailedSidebar.tsx
@@ -1,6 +1,39 @@
+import { Item, Segment } from 'semantic-ui-react';
+
 const EventDetailedSidebar = () => {
   return (
-    <div>EventDetailedSidebar</div>
-  )
-}
-export default EventDetailedSidebar
+    <>
+      <Segment
+        textAlign='center'
+        style={{ border: 'none' }}
+        attached='top'
+        secondary
+        inverted
+        color='teal'
+      >
+        2 People Going
+      </Segment>
+      <Segment attached>
+        <Item.Group relaxed divided>
+          <Item style={{ position: 'relative' }}>
+            <Item.Image size='tiny' src='/user.png' />
+            <Item.Content verticalAlign='middle'>
+              <Item.Header as='h3'>
+                <span>Tom</span>
+              </Item.Header>
+            </Item.Content>
+          </Item>
+          <Item style={{ position: 'relative' }}>
+            <Item.Image size='tiny' src='/user.png' />
+            <Item.Content verticalAlign='middle'>
+              <Item.Header as='h3'>
+                <span>Bob</span>
+              </Item.Header>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Segment>
+    </>
+  );
+};
+export default EventDetailedSidebar;
