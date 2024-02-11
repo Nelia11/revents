650fed9f41c808fef3b64b7cada3fc22c8591d6c
Nelia11 <levit.nelia@icloud.com>
2024-02-11 13:41:12 +0100

    Add form validation

--- src/features/events/form/EventForm.tsx
+++ src/features/events/form/EventForm.tsx
@@ -1,20 +1,19 @@
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { Button, Form, Header, Segment } from 'semantic-ui-react';
 import { useAppSelector } from '../../../app/store/store';
-import { useDispatch } from 'react-redux';
-import { createEvent, updateEvent } from '../eventsSlice';
-import { createId } from '@paralleldrive/cuid2';
 import { FieldValues, useForm } from 'react-hook-form';
 
 const EventForm = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid, isSubmitting },
+  } = useForm({ mode: 'onTouched' });
 
   let { id } = useParams();
   const event = useAppSelector((state) =>
     state.events.events.find((e) => e.id === id)
   );
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
 
   const onSubmit = (data: FieldValues) => {
     console.log(data);
@@ -35,41 +34,56 @@ const EventForm = () => {
 
   return (
     <Segment clearing>
-      <Header content={event ? 'Update event' : 'Create event'} />
+      <Header content='Event details' sub color='teal' />
       <Form onSubmit={handleSubmit(onSubmit)}>
         <Form.Input
           placeholder='Event title'
           defaultValue={event?.title || ''}
-          {...register('title')}
+          {...register('title', { required: 'Title is required' })}
+          error={errors.title && errors.title.message}
         />
         <Form.Input
           placeholder='Category'
           defaultValue={event?.category || ''}
-          {...register('category')}
+          {...register('category', { required: 'Category is required' })}
+          error={errors.category && errors.category.message}
         />
-        <Form.Input
+        <Form.TextArea
           placeholder='Description'
           defaultValue={event?.description || ''}
-          {...register('description')}
+          {...register('description', { required: 'Description is required' })}
+          error={errors.description && errors.description.message}
         />
+        <Header sub content='Location details' color='teal' />
         <Form.Input
           placeholder='City'
           defaultValue={event?.city || ''}
-          {...register('city')}
+          {...register('city', { required: 'City is required' })}
+          error={errors.city && errors.city.message}
         />
         <Form.Input
           placeholder='Street'
           defaultValue={event?.street || ''}
-          {...register('street')}
+          {...register('street', { required: 'Street is required' })}
+          error={errors.street && errors.street.message}
         />
         <Form.Input
           type='date'
           placeholder='Date'
           defaultValue={event?.date || ''}
-          {...register('date')}
+          {...register('date', { required: 'Date is required' })}
+          error={errors.date && errors.date.message}
         />
-        <Button type='submit' floated='right' positive content='Submit' />
         <Button
+          loading={isSubmitting}
+          disabled={!isValid}
+          type='submit'
+          floated='right'
+          positive
+          content='Submit'
+        />
+        <Button
+          disabled={isSubmitting}
           as={Link}
           to={'/events'}
           type='button'
