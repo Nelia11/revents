8a3aee504c2e816fb9c463c00b9fbdb1a47689a1
Nelia11 <levit.nelia@icloud.com>
2024-04-03 14:03:05 +0200

    Add firebase configurations

---
+++ src/app/config/firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: 'revents-7c80f.firebaseapp.com',
+  projectId: 'revents-7c80f',
+  storageBucket: 'revents-7c80f.appspot.com',
+  messagingSenderId: '245990326622',
+  appId: '1:245990326622:web:a222d70f315b4add4b4fcf',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
