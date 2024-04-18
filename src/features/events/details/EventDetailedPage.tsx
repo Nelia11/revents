1e37bab762841a18c7885601998cf1f40ed98a39
Nelia11 <levit.nelia@icloud.com>
2024-04-18 15:59:53 +0200

    Use the hook to get a single document

--- src/features/events/details/EventDetailedPage.tsx
+++ src/features/events/details/EventDetailedPage.tsx
@@ -4,39 +4,26 @@ import EventDetailedInfo from './EventDetailedInfo';
 import EventDetailedChat from './EventDetailedChat';
 import EventDetailedSidebar from './EventDetailedSidebar';
 import { useParams } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from '../../../app/store/store';
-import { useEffect, useState } from 'react';
-import { doc, onSnapshot } from 'firebase/firestore';
-import { db } from '../../../app/config/firebase';
+import { useAppSelector } from '../../../app/store/store';
+import { useEffect } from 'react';
 import { actions } from '../eventsSlice';
-import { toast } from 'react-toastify';
 import LoadingComponent from '../../../app/layout/nav/LoadingComponent';
+import { useFirestore } from '../../../app/hooks/firestore/useFirestore';
 
 const EventDetailedPage = () => {
   const { id } = useParams();
   const event = useAppSelector((state) =>
     state.events.data.find((e) => e.id === id)
   );
-  const dispatch = useAppDispatch();
-  const [isLoading, setIsLoading] = useState(true);
+  const { status } = useAppSelector((state) => state.events);
+  const { loadDocument } = useFirestore('events');
 
   useEffect(() => {
     if (!id) return;
-    const unsubscribe = onSnapshot(doc(db, 'events', id), {
-      next: (doc) => {
-        dispatch(actions.success({ id: doc.id, ...doc.data } as any));
-        setIsLoading(false);
-      },
-      error: (err) => {
-        console.error(err);
-        toast.error(err.message);
-        setIsLoading(false);
-      },
-    });
-    return () => unsubscribe();
-  }, [id, dispatch]);
+    loadDocument(id, actions);
+  }, [id, loadDocument]);
 
-  if (isLoading) return <LoadingComponent />;
+  if (status === 'loading') return <LoadingComponent />;
 
   if (!event) return <h2>Event not found</h2>;
 
