2148bcdf9cd077b0af75dafecfe8a62e1c2af12c
Nelia11 <levit.nelia@icloud.com>
2024-08-25 14:43:13 +0200

    Inplement signout

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -7,7 +7,7 @@ import { useEffect } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
 import { useAppDispatch } from '../store/store';
 import { auth } from '../config/firebase';
-import { signIn } from '../../features/auth/authSlice';
+import { logout, signIn } from '../../features/auth/authSlice';
 
 const App = () => {
   const location = useLocation();
@@ -18,6 +18,8 @@ const App = () => {
       next: user => {
         if(user) {
           dispatch(signIn(user))
+        } else {
+          dispatch(logout())
         }
       },
       error: error => console.log(error),
